773b2e37f623bef203d8e3df145832ca60b0b14d
Stav Barak <stav@lynx.md>
2019-03-11 15:22:44 +0200

    more efficient algorithm

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -28,81 +28,59 @@ class MainView extends Component {
       })   
   }
 
-  // functions to detect most isolated
+  mapIsolatedAgentsToCountries = (missions) => {
+    // map all agents
+    const agentsMap = {};
 
-  getMissionsByCountry = (missions) => {
-    let missionsByCountry = {};
-    if(missions){
-      for(let mission of missions) {
-        if (missionsByCountry[ mission.country ] instanceof Array) {
-          missionsByCountry[ mission.country ].push(mission);
-        } else {
-          missionsByCountry[ mission.country ] = [ mission ];
-        }
-      }
-    }
-    
-    return missionsByCountry;
-  }
-
-  getIsolatedAgents = (missions) => {
-    const agentCounter = {};  
-    const isolatedAgents = [];
-
-    // map all agents to a counter object to see which are isolated
     for (let mission of missions) {
-      if (agentCounter[mission.agent]){
-        agentCounter[mission.agent] ++;
+      if(agentsMap[mission.agent]) {
+        agentsMap[mission.agent].push(mission.country);
       }
       else {
-        agentCounter[mission.agent] = 1;
+        agentsMap[mission.agent] = [mission.country];
       }
     }
-    
-    // get all isolated agents from the agents map
-    for (let agent in agentCounter) {
-      if (agentCounter[agent] === 1) {
-        isolatedAgents.push(agent);
+
+    // remove non-isolated agents
+    for (let agent in agentsMap) {
+      if (agentsMap[agent].length > 1) {
+        delete agentsMap[agent];
       }
     }
-    return isolatedAgents;
+
+    return agentsMap;
   }
 
-  // check which country has the biggest intersection with the isolated agents array
+  getMostIsolatedCountry(missions) {
+    const countryCounter = {};
+    const agentsMap = this.mapIsolatedAgentsToCountries(missions);
 
-  getMostIsolated = (missions) => {
-    const missionsBycountry = this.getMissionsByCountry(missions);
-    const isolatedAgents = this.getIsolatedAgents(missions);
-    let countyWinnerBoard = {};
-   
-    for (let country in missionsBycountry) {        
-        for (let mission in missionsBycountry[country]){
-          if (isolatedAgents.indexOf(missionsBycountry[country][mission].agent) > -1) {
-            if (countyWinnerBoard[country]){
-              countyWinnerBoard[country] ++;
-            }
-            else {
-              countyWinnerBoard[country] = 1
-            }
-          }
-        }        
+    for (let agent in agentsMap) {
+      let country = agentsMap[agent][0];
+      if (countryCounter[country]) {
+        countryCounter[country] ++;
+      }
+      else {
+        countryCounter[country] = 1;
+      }     
     }
 
     let winner; 
-    if (Object.keys(countyWinnerBoard).length > 0){
-      winner =  Object.keys(countyWinnerBoard).reduce((a, b) => countyWinnerBoard[a] > countyWinnerBoard[b] ? a : b);
+    if (Object.keys(countryCounter).length > 0){
+      winner =  Object.keys(countryCounter).reduce((a, b) => 
+      countryCounter[a] > countryCounter[b] ? a : b);
     }
 
     return winner;
   }
 
+
   render () {
     const { missions, loaded } = this.state;
-    
     return (
       <div>
         {loaded ? <ResultsTable listOfResults={ missions }/> : '...'}
-        <Winner mostIsolatedCountry = {this.getMostIsolated(missions)} />
+        <Winner mostIsolatedCountry = {this.getMostIsolatedCountry(missions)} />
       </div>
     )
   }
